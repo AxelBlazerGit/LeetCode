18f0b7f7fd0d39fd953d2f1c78e5dbf01cfad0d0
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-05-05 02:10:39 +0530

    Create 1910.Remove All Occurences of a Substring.py

---
+++ medium/1910.Remove All Occurences of a Substring.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        while part in s:
+            idx = s.find(part)
+            if idx != -1:
+                s = s[:idx] + s[idx+len(part):]
+        return s
