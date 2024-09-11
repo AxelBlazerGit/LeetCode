88d16da565ae18c74e937a28448bb93de61c167a
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-09-11 10:43:24 +0530

    Create 2220. Minimum Bit Flips to  Convert Number.py

--- easy/2220. Minimum Bit Flips to  Convert Number.py
+++ easy/2220. Minimum Bit Flips to  Convert Number.py
@@ -1,12 +1,19 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        start,goal=bin(start)[2:][::-1],bin(goal)[2:][::-1]
         flips=0
-        mn,mx=(start,goal) if len(start)<=len(goal) else (goal,start)
-        for i in range(len(mn)):
-            if mn[i]!=mx[i]:
-                flips+=1
-        for i in range(len(mn),len(mx)):
-            if mx[i]=='1':
-                flips+=1
+        start^=goal
+        while start:
+            flips+=start&1
+            start >>=1
         return flips
+        # start,goal=bin(start)[2:][::-1],bin(goal)[2:][::-1]
+        # flips=0
+        # mn,mx=(start,goal) if len(start)<=len(goal) else (goal,start)
+        # for i in range(len(mn)):
+        #     if mn[i]!=mx[i]:
+        #         flips+=1
+        # for i in range(len(mn),len(mx)):
+        #     if mx[i]=='1':
+        #         flips+=1
+        # return flips
+
