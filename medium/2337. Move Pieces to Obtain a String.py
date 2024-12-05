9194bf6ba331af87cee56cff7d407742a031d42f
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-12-05 16:55:07 +0530

    Create 2337. Move Pieces to Obtain a String.py

---
+++ medium/2337. Move Pieces to Obtain a String.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        start=[(i,idx) for idx,i in enumerate(start) if i in 'LR']
+        target=[(i,idx) for idx,i in enumerate(target) if i in 'LR']
+
+        if len(start)!=len(target):
+            return False
+        
+        for i in range(len(start)):
+            if start[i][0]!=target[i][0]:
+                return False
+            
+            if start[i][0]=='L':
+                if start[i][1]<target[i][1]:
+                    return False
+
+            if start[i][0]=='R':
+                if start[i][1]>target[i][1]:
+                    return False
+            
+        return True
