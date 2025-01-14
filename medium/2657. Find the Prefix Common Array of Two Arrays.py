a5ecd9da6dbb1b9b082105974f62febaeca4718a
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2025-01-14 19:19:15 +0530

    Update 2657. Find the Prefix Common Array of Two Arrays.py

--- medium/2657. Find the Prefix Common Array of Two Arrays.py
+++ medium/2657. Find the Prefix Common Array of Two Arrays.py
@@ -1,9 +1,31 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        hash=defaultdict(int)
-        pca=[]
+        hash=[0]*(len(A)+1)
+        pca=[0]*len(A)
+        cur=0
         for i in range(len(A)):
             hash[A[i]]+=1
+            if hash[A[i]]==2:
+                cur+=1
             hash[B[i]]+=1
-            pca.append(sum(1 for k in hash if hash[k]==2))
+            if hash[B[i]]==2:
+                cur+=1
+            pca[i]=cur
+            
         return pca
+# 1 3 2 4
+# 3 1 2 4
+# 0 1 0 1 0
+# 0 2 0 2 0
+# 0 2 2 2 0
+# 0 
+
+# class Solution:
+#     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+#         hash=defaultdict(int)
+#         pca=[]
+#         for i in range(len(A)):
+#             hash[A[i]]+=1
+#             hash[B[i]]+=1
+#             pca.append(sum(1 for k in hash if hash[k]==2))
+#         return pca
